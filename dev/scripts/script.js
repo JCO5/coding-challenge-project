c5ea36e6b7838d78483ea5fcb9618e38d3d65559
DolphStarX <nick.birkus@gmail.com>
2023-08-03 17:59:48 -0700

    header improvements

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -6,8 +6,21 @@
 const hamburgerIcon = document.getElementById('hamburger-icon');
 const mainNav = document.getElementById('main-nav');
 
+// Function to close the hamburger menu
+function closeHamburgerMenu() {
+  mainNav.classList.remove('active');
+}
+
 // Add an event listener to the hamburger icon
 hamburgerIcon.addEventListener('click', () => {
   // Toggle the 'active' class on the navigation
   mainNav.classList.toggle('active');
+});
+
+// Add a resize event listener to close the hamburger menu when screen size is greater than 500px
+window.addEventListener('resize', () => {
+  const screenWidth = window.innerWidth;
+  if (screenWidth > 500) {
+    closeHamburgerMenu();
+  }
 });
